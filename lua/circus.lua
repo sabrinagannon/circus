c17b243153d86e500d6e1b2293b47a55246814ba
Sabrina Gannon <sabrinagannon@icloud.com>
2021-09-13 19:33:57 -0400

    Git history based file browser
    Fixed! It works!

--- lua/circus.lua
+++ lua/circus.lua
@@ -1,24 +1,38 @@
 local api = vim.api
 local buf, win
+local position = 0 -- Param to store information on if we want an older or newer state
+
+-- Center function for displaying a centered title text
+local function center(str)
+  local width = api.nvim_win_get_width(0)
+  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
+  return string.rep(' ', shift) .. str
+end
 
 local function open_window()
-  buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
+  buf = vim.api.nvim_create_buf(false, true)
+  local border_buf = vim.api.nvim_create_buf(false, true)
 
-  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'whid')
 
-  -- get dimensions
-  local width = api.nvim_get_option("columns")
-  local height = api.nvim_get_option("lines")
+  local width = vim.api.nvim_get_option("columns")
+  local height = vim.api.nvim_get_option("lines")
 
-  -- calculate our floating window size
   local win_height = math.ceil(height * 0.8 - 4)
   local win_width = math.ceil(width * 0.8)
-
-  -- and its starting position
   local row = math.ceil((height - win_height) / 2 - 1)
   local col = math.ceil((width - win_width) / 2)
 
-  -- set some options
+  local border_opts = {
+    style = "minimal",
+    relative = "editor",
+    width = win_width + 2,
+    height = win_height + 2,
+    row = row - 1,
+    col = col - 1
+  }
+
   local opts = {
     style = "minimal",
     relative = "editor",
@@ -28,69 +42,46 @@ local function open_window()
     col = col
   }
 
-  -- Border window optons and buffer creation
-  local border_opts = {
-          style = "minimal",
-          relative = "editor",
-          width = win_width + 2,
-          height = win_height + 2,
-          row = row - 1,
-          col = col - 1
-  }
-  local border_buf = api.nvim_create_buf(false, true)
-
   local border_lines = { '╔' .. string.rep('═', win_width) .. '╗' }
   local middle_line = '║' .. string.rep(' ', win_width) .. '║'
   for i=1, win_height do
-          table.insert(border_lines, middle_line)
+    table.insert(border_lines, middle_line)
   end
   table.insert(border_lines, '╚' .. string.rep('═', win_width) .. '╝')
+  vim.api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
 
-  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
-  -- set bufer's (border_buf) lines from first line (0) to last (-1)
--- ignoring out-of-bounds error (false) with lines (border_lines)
-
-  -- and finally create it with buffer attached
-  local border_win = api.nvim_open_win(border_buf, true, border_opts) -- Border window
+  local border_win = vim.api.nvim_open_win(border_buf, true, border_opts)
   win = api.nvim_open_win(buf, true, opts)
-  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf) -- Vim autocommand to ensure we close both windows together, always
+  api.nvim_command('au BufWipeout <buffer> exe "silent bwipeout! "'..border_buf)
+
+  vim.api.nvim_win_set_option(win, 'cursorline', true)
+  api.nvim_buf_set_lines(buf, 0, -1, false, { center('Circus'), '', ''})
+  api.nvim_buf_add_highlight(buf, -1, 'CircusHeader', 0, 0, -1)
 end
 
 -- Function to get data to show in the window we created above, updates the view repeatedly
-local position = 0 -- Param to store information on if we want an older or newer state
-local function update_view()
+local function update_view(direction)
+  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
   position = position + direction
-  if position < 0 then position = 0 end -- HEAD~0 is the newest state
+  if position < 0 then position = 0 end
 
-  -- we will use vim systemlist function which run shell
-  -- command and return result as list
-  local result = vim.fn.systemlist('git diff-tree --no-commit-id --name-only -r  HEAD~'..position)
+  local result = vim.api.nvim_call_function('systemlist', {
+      'git diff-tree --no-commit-id --name-only -r HEAD~'..position
+    })
 
-  -- with small indentation results will look better
+  if #result == 0 then table.insert(result, '') end
   for k,v in pairs(result) do
     result[k] = '  '..result[k]
   end
 
-  api.nvim_buf_set_lines(buf, 0, -1, false, {
-      center('How many plates are spinning?'), -- Centered plugin title
-      center('HEAD~'..position), -- Centered indication of where we are in the history
-      ''
-    })
-
+  api.nvim_buf_set_lines(buf, 1, 2, false, {center('HEAD~'..position)})
   api.nvim_buf_set_lines(buf, 3, -1, false, result)
 
-  api.nvim_buf_add_highlight(buf, -1, 'CircusHeader', 0, 0, -1)
   api.nvim_buf_add_highlight(buf, -1, 'circusSubHeader', 1, 0, -1)
 
   api.nvim_buf_set_option(buf, 'modifiable', false)
 end
 
--- Center function for displaying a centered title text
-local function center(str)
-  local width = api.nvim_win_get_width(0)
-  local shift = math.floor(width / 2) - math.floor(string.len(str) / 2)
-  return string.rep(' ', shift) .. str
-end
 
 -- Everything below here adds interactivity to the plugin
 local function close_window()
